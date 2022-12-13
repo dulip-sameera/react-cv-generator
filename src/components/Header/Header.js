9d0eed0fbeab5beafedbb2faf1973df8d22918ba
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-13 16:41:16 +0530

    add styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const LayoutDiv = styled.div`
+  grid-row: 1/2;
+  display: flex;
+  align-items: center;
+
+  padding: 0 5%;
+  background: ${({ theme }) => theme.colors.headerBg};
+`;
+
+const BrandName = styled.h1`
+  font-family: "Yantramanav";
+  font-weight: 900;
+  font-size: 2.5rem;
+  letter-spacing: -0.05em;
+  color: ${({ theme }) => theme.colors.brandName};
+
+  &:hover {
+    cursor: default;
+  }
+`;
 
 export class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <LayoutDiv>
+        <BrandName>CV Generator</BrandName>
+      </LayoutDiv>
+    );
   }
 }
 
