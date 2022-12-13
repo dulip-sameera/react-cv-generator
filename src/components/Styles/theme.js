68bcfc84f4b51073b4dba6ebb193a4fb67104b08
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-13 16:22:47 +0530

    create theme object

---
+++ src/components/Styles/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    bodyBg: "#282a3a",
+    headerBg: "#101223",
+    brandName: "#f3f4f8",
+    footerBg: "#101223",
+    footerText: "#f3f4f8",
+    btnText: "#fff",
+    btnBg: "#282a3a",
+    textPrimary: "#404252",
+    textSecondary: "#282a3a",
+    red: "#ff4949",
+    green: "#13ce66",
+    blue: "#2d8eff",
+    yellow: "#ffcc3d",
+  },
+};
+
+export default theme;
