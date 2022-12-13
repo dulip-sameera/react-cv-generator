4e827ac96b3afdb0583b4e9a316220ce12fc4375
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-13 16:47:23 +0530

    update App: add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Header } from "./components";
+import { Footer, Header } from "./components";
 
 export class App extends Component {
   render() {
     return (
       <>
         <Header />
+        <Footer />
       </>
     );
   }
