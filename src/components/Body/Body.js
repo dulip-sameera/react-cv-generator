2e9710ecb2f102163c3594d346d55b87f01df98b
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-14 09:19:07 +0530

    add layout styles

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const LayoutDiv = styled.div`
+  grid-row: 2/3;
+
+  padding: 1.25rem 5%;
+  background: ${({ theme }) => theme.colors.bodyBg};
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2.5rem;
+`;
 
 export class Body extends Component {
   render() {
-    return <div>Body</div>;
+    return (
+      <LayoutDiv>
+        <div>Body</div>
+      </LayoutDiv>
+    );
   }
 }
 
