1e184bd7d015b11efb703ed723f32f8f00ad3a0a
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-14 18:58:05 +0530

    add layout and colors

--- src/components/Body/Form/Form.js
+++ src/components/Body/Form/Form.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const LayoutDiv = styled.div`
+  grid-column: 1/2;
+  padding: 1.25rem;
+
+  background: ${({ theme }) => theme.colors.grey};
+  border-radius: 10px;
+`;
 
 export class Form extends Component {
   render() {
-    return <div>Form</div>;
+    const { data, experience, education } = this.props.info;
+    return (
+      <LayoutDiv>
+        <div>Form</div>
+      </LayoutDiv>
+    );
   }
 }
 
