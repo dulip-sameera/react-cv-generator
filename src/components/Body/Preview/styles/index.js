ac513a2ef7a11329a444d0d2bb605ad3304e37e5
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-15 16:23:28 +0530

    create index.js for preview styles

---
+++ src/components/Body/Preview/styles/index.js
@@ -0,0 +1,5 @@
+import SectionTitle from "./SectionTitle.styles";
+import SubTitle from "./SubTitle.styles";
+import Description from "./Description.styles";
+
+export { SectionTitle, SubTitle, Description };
