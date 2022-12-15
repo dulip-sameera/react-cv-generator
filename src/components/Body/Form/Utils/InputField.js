1b5e7d5509e356c9c43f30fa3dc47a30e0fc55da
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-15 15:34:35 +0530

    add text area

--- src/components/Body/Form/Utils/InputField.js
+++ src/components/Body/Form/Utils/InputField.js
@@ -18,9 +18,42 @@ const Input = styled.input`
   }
 `;
 
+const TextArea = styled.textarea`
+  color: ${({ theme }) => theme.colors.textPrimary};
+  background: ${({ theme }) => theme.colors.darkGrey};
+  padding: 0.3rem 0.6rem;
+  margin-bottom: 0.7rem;
+  border-radius: 5px;
+  font-size: 1.25rem;
+  font-weight: 700;
+  outline: none;
+  border: none;
+  resize: none;
+  width: 100%;
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.textPrimary};
+  }
+`;
+
 export class InputField extends Component {
   render() {
-    const { id, type, maxLength, placeholder, onChangeFn } = this.props;
+    const { id, type, maxLength, placeholder, onChangeFn, accept } = this.props;
+
+    if (type === "file") {
+      return (
+        <Input id={id} type={type} accept={accept} onChange={onChangeFn} />
+      );
+    }
+
+    if (type === "textArea") {
+      return (
+        <TextArea
+          id={id}
+          placeholder={placeholder}
+          onChange={onChangeFn}
+        ></TextArea>
+      );
+    }
 
     return (
       <Input
