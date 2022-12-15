e3acf03a21e65ba907931aa8afb4121f0d1e72ec
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-15 13:40:54 +0530

    add the first three input fields

--- src/components/Body/Form/Personal.js
+++ src/components/Body/Form/Personal.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 import SectionTitle from "./styles/SectionTitle.styles";
+import { InputField } from "./Utils";
 
 export class Personal extends Component {
   render() {
-    const { data } = this.props;
+    const { data, onChangeFn } = this.props;
     console.log(data);
-
     return (
       <div>
         <SectionTitle>Personal Information</SectionTitle>
+        <InputField
+          id="firstName"
+          type="text"
+          maxLength="100"
+          onChangeFn={onChangeFn}
+          placeholder="First Name"
+        />
+        <InputField
+          id="lastName"
+          type="text"
+          maxLength="100"
+          onChangeFn={onChangeFn}
+          placeholder="Last Name"
+        />
+
+        <InputField
+          id="position"
+          type="text"
+          maxLength="100"
+          onChangeFn={onChangeFn}
+          placeholder="Position"
+        />
       </div>
     );
   }
