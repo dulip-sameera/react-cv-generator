d832a1580dfd2b0d2727fd835ed499b50bfad8ca
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-15 12:27:26 +0530

    add a bottom margin

--- src/components/Body/Form/styles/SectionTitle.styles.js
+++ src/components/Body/Form/styles/SectionTitle.styles.js
@@ -4,6 +4,7 @@ const SectionTitle = styled.h1`
   font-size: 1.5rem;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.textSecondary};
+  margin-bottom: 0.3rem;
 
   &:hover {
     cursor: default;
