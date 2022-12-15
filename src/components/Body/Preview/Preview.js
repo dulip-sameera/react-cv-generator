ce27f68c2665c42eb4ed651b420f8ed375339ac5
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-15 14:31:43 +0530

    add header

--- src/components/Body/Preview/Preview.js
+++ src/components/Body/Preview/Preview.js
@@ -1,19 +1,34 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import Header from "./Header";
 
 const LayoutDiv = styled.div`
+  background: ${({ theme }) => theme.colors.grey};
+
   grid-column: 2/3;
 
-  background: ${({ theme }) => theme.colors.grey};
+  display: grid;
+  grid-template-rows: 120px 1fr;
+`;
+
+const BodyDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 300px;
 `;
 
 export class Preview extends Component {
   render() {
     const { info } = this.props;
+    const { personalInfo } = info;
+    const { firstName, lastName, position } = personalInfo;
 
     return (
       <LayoutDiv>
-        <div>Preview</div>
+        <Header name={`${firstName} ${lastName}`} position={position} />
+        <BodyDiv>
+          <div>Hello</div>
+          <div>World Create Problems, Humans to solve</div>
+        </BodyDiv>
       </LayoutDiv>
     );
   }
