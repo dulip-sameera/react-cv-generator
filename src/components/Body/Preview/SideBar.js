f5a5e1d032cb0cd7a408f1bb1f8936790703e88f
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-15 16:22:23 +0530

    create side bar

--- src/components/Body/Preview/SideBar.js
+++ src/components/Body/Preview/SideBar.js
@@ -1,8 +1,56 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import defaultImage from "../../../assets/profile_pic.png";
+import { Description, SectionTitle, SubTitle } from "./styles";
+
+const LayoutDiv = styled.div`
+  grid-column: 2/3;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
+  background: ${({ theme }) => theme.colors.grey};
+`;
+
+const Image = styled.img`
+  width: 220px;
+  height: 280px;
+  margin-bottom: 20px;
+`;
+
+const Container = styled.div`
+  width: 220px;
+
+  & > *:hover {
+    cursor: default;
+  }
+`;
 
 export class SideBar extends Component {
   render() {
-    return <div>SideBar</div>;
+    let { image, address, email, phoneNumber } = this.props;
+
+    if (!image) {
+      image = defaultImage;
+    }
+
+    return (
+      <LayoutDiv>
+        <Image src={image} />
+        <Container>
+          <SectionTitle>Personal Details</SectionTitle>
+
+          <SubTitle>Address</SubTitle>
+          <Description>{address}</Description>
+
+          <SubTitle>Phone Number</SubTitle>
+          <Description>{phoneNumber}</Description>
+
+          <SubTitle>Email</SubTitle>
+          <Description>{email}</Description>
+        </Container>
+      </LayoutDiv>
+    );
   }
 }
 
