a5366fa0a43e584d48778610fac9027b50cd7383
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-15 12:28:05 +0530

    Style the input field

--- src/components/Body/Form/Utils/InputField.js
+++ src/components/Body/Form/Utils/InputField.js
@@ -2,14 +2,34 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const Input = styled.input`
-  padding: 0.3rem 0.6rem;
-  font-weight: 700;
   color: ${({ theme }) => theme.colors.textPrimary};
+  background: ${({ theme }) => theme.colors.darkGrey};
+  padding: 0.3rem 0.6rem;
+  border-radius: 5px;
+  font-size: 1.25rem;
+  font-weight: 700;
+  outline: none;
+  border: none;
+  width: 100%;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.textPrimary};
+  }
 `;
 
 export class InputField extends Component {
   render() {
-    return <div>InputField</div>;
+    const { id, type, maxLength, placeholder, onchangeFn } = this.props;
+
+    return (
+      <Input
+        id={id}
+        type={type}
+        maxlength={maxLength}
+        placeholder={placeholder}
+        onChange={onchangeFn}
+      />
+    );
   }
 }
 
