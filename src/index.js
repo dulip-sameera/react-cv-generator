1a798d95ab15e9a57b4de2514c41d9897d63d042
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 12:00:28 +0530

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import { GlobalStyles, theme } from "./components/Styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <App />
+  </ThemeProvider>
 );
