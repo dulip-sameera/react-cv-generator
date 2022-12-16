df0e9cf221584106ae4d7061ff77ce563d4d8c19
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 08:28:52 +0530

    create experience item component

---
+++ src/components/Body/Form/ExperienceItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ExperienceItem extends Component {
+  render() {
+    return <div>ExperienceItem</div>;
+  }
+}
+
+export default ExperienceItem;
