bdb463ab6566709edbb07fb913290e54b5867d25
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 11:59:50 +0530

    remove the test codes and make it work with the state changes

--- src/components/Body/Form/Experience.js
+++ src/components/Body/Form/Experience.js
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
 import ExperienceItem from "./ExperienceItem";
 import SectionTitle from "./styles/SectionTitle.styles";
+import { Button } from "./Utils";
 
 export class Experience extends Component {
   render() {
     const { data, onChangeFn, deleteFn, addFn } = this.props;
-    const testOb = {
-      id: "1",
-      position: "Senior Web Developer",
-      company: "Facebook Inc.",
-      address: "Menlo Park",
-      from: "2012",
-      to: "2015",
-    };
 
     return (
       <div>
         <SectionTitle>Experience</SectionTitle>
-        <ExperienceItem
-          id={testOb.id}
-          position={testOb.position}
-          company={testOb.company}
-          address={testOb.address}
-          from={testOb.from}
-          to={testOb.to}
-          deleteFn={deleteFn}
-          key={testOb.id}
-          onChangeFn={onChangeFn}
-        />
+
+        {data.map((item) => {
+          return (
+            <ExperienceItem
+              id={item.id}
+              position={item.position}
+              company={item.company}
+              address={item.address}
+              from={item.from}
+              to={item.to}
+              deleteFn={deleteFn}
+              key={item.id}
+              onChangeFn={onChangeFn}
+            />
+          );
+        })}
+
+        <Button text="Add" onClickFn={addFn} type="experience" />
       </div>
     );
   }
