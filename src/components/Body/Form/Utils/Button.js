57bf4ed51d8f91c4ac09b11530835de920bd2c46
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 10:52:18 +0530

    add default button

--- src/components/Body/Form/Utils/Button.js
+++ src/components/Body/Form/Utils/Button.js
@@ -1,10 +1,51 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Btn = styled.button`
+  width: 100%;
+  background: ${({ theme }) => theme.colors.btnBg};
+  color: ${({ theme }) => theme.colors.btnText};
+  border: none;
+  padding: 5px;
+  font-size: 1.25rem;
+  font-weight: 700;
+  border-radius: 5px;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.btnBgHover};
+    cursor: pointer;
+  }
+`;
 
 export class Button extends Component {
   render() {
+    const { text, onClickFn, id, type } = this.props;
+
+    if (type === "experience") {
+      return (
+        <>
+          <Btn onClick={onClickFn} id="experience">
+            {text}
+          </Btn>
+        </>
+      );
+    }
+
+    if (type === "education") {
+      return (
+        <>
+          <Btn onClick={onClickFn} id="education">
+            {text}
+          </Btn>
+        </>
+      );
+    }
+
     return (
       <>
-        <button>Delete</button>
+        <Btn onClick={onClickFn} id={id}>
+          {text}
+        </Btn>
       </>
     );
   }
