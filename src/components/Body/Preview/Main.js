6d0ec80fc9c43b672db73f44d26bbefe83ed84ca
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 08:12:40 +0530

    Add description section

--- src/components/Body/Preview/Main.js
+++ src/components/Body/Preview/Main.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Description, SectionTitle } from "./styles";
 
 const LayoutDiv = styled.div`
   grid-column: 1/2;
   background: ${({ theme }) => theme.colors.white};
+  padding: 30px 20px;
+
+  & > *:hover {
+    cursor: default;
+  }
 `;
 
 export class Main extends Component {
   render() {
+    const { description } = this.props;
+
     return (
       <LayoutDiv>
-        <div>Main</div>
+        <SectionTitle>Description</SectionTitle>
+        <Description>{description}</Description>
       </LayoutDiv>
     );
   }
