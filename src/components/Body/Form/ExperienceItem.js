4607fe7a8f03a918cfe795f1ea44f07ba634aa44
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 10:22:39 +0530

    add input fields

--- src/components/Body/Form/ExperienceItem.js
+++ src/components/Body/Form/ExperienceItem.js
@@ -1,8 +1,69 @@
 import React, { Component } from "react";
+import { InputField } from "./Utils";
 
 export class ExperienceItem extends Component {
   render() {
-    return <div>ExperienceItem</div>;
+    const { id, position, company, address, from, to, deleteFn, onChangeFn } =
+      this.props;
+
+    return (
+      <div>
+        <InputField
+          type="text"
+          maxLength="30"
+          placeholder="Position"
+          dataField="position"
+          dataList="experience"
+          id={id}
+          onChangeFn={onChangeFn}
+          list
+        />
+
+        <InputField
+          type="text"
+          maxLength="30"
+          placeholder="Company"
+          dataField="company"
+          dataList="experience"
+          id={id}
+          onChangeFn={onChangeFn}
+          list
+        />
+
+        <InputField
+          type="text"
+          maxLength="250"
+          placeholder="Address"
+          dataField="address"
+          dataList="experience"
+          id={id}
+          onChangeFn={onChangeFn}
+          list
+        />
+
+        <InputField
+          type="text"
+          maxLength="4"
+          placeholder="From"
+          dataField="from"
+          dataList="experience"
+          id={id}
+          onChangeFn={onChangeFn}
+          list
+        />
+
+        <InputField
+          type="text"
+          maxLength="4"
+          placeholder="To"
+          dataField="to"
+          dataList="experience"
+          id={id}
+          onChangeFn={onChangeFn}
+          list
+        />
+      </div>
+    );
   }
 }
 
