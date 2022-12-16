fe7df19c4df86a3af1d71a3a58464b743f28e363
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 08:29:10 +0530

    add experience component

--- src/components/Body/Form/Experience.js
+++ src/components/Body/Form/Experience.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import ExperienceItem from "./ExperienceItem";
+import SectionTitle from "./styles/SectionTitle.styles";
 
 export class Experience extends Component {
   render() {
-    return <div>Experience</div>;
+    const { data, onChangeFn, deleteFn, addFn } = this.props;
+    const testOb = {
+      id: "1",
+      position: "Senior Web Developer",
+      company: "Facebook Inc.",
+      address: "Menlo Park",
+      from: "2012",
+      to: "2015",
+    };
+
+    return (
+      <div>
+        <SectionTitle>Experience</SectionTitle>
+        <ExperienceItem
+          id={testOb.id}
+          position={testOb.position}
+          company={testOb.company}
+          address={testOb.address}
+          from={testOb.from}
+          to={testOb.to}
+          deleteFn={deleteFn}
+          key={testOb.id}
+        />
+      </div>
+    );
   }
 }
 
