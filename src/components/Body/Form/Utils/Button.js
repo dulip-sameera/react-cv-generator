b998b03dfb7b4245d35d7f7daf3f11df725c7682
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 10:23:10 +0530

    create button component

---
+++ src/components/Body/Form/Utils/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Button extends Component {
+  render() {
+    return (
+      <>
+        <button>Delete</button>
+      </>
+    );
+  }
+}
+
+export default Button;
