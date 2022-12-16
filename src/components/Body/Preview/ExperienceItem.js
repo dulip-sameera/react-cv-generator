ab54d85ea38614f05f34178def0db1ac3115911f
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 14:48:41 +0530

    style and set the props

--- src/components/Body/Preview/ExperienceItem.js
+++ src/components/Body/Preview/ExperienceItem.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Description, SubTitle } from "./styles";
+
+const LayoutDiv = styled.div`
+  display: flex;
+  gap: 40px;
+  margin-bottom: 10px;
+`;
+
+const Period = styled(SubTitle)``;
+const Position = styled(SubTitle)``;
+
+const Company = styled(Description)`
+  margin-bottom: 0px;
+`;
+
+const Address = styled(Description)`
+  margin-bottom: 0px;
+`;
 
 export class ExperienceItem extends Component {
   render() {
-    return <div>ExperienceItem</div>;
+    const { from, to, address, position, company } = this.props;
+
+    return (
+      <LayoutDiv>
+        <Period>
+          {from} - {to}
+        </Period>
+        <div>
+          <Position>{position}</Position>
+          <Company>{company}</Company>
+          <Address>{address}</Address>
+        </div>
+      </LayoutDiv>
+    );
   }
 }
 
