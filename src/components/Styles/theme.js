fc7100ada32892493a292bbb0ee374b726d70357
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-16 16:23:31 +0530

    add red, green, yellow shades

--- src/components/Styles/theme.js
+++ src/components/Styles/theme.js
@@ -10,6 +10,42 @@ const black = {
   900: "#08080c",
 };
 
+const red = {
+  100: "#ffdbdb",
+  200: "#ffb6b6",
+  300: "#ff9292",
+  400: "#ff6d6d",
+  500: "#ff4949",
+  600: "#cc3a3a",
+  700: "#992c2c",
+  800: "#661d1d",
+  900: "#330f0f",
+};
+
+const green = {
+  100: "#d0f5e0",
+  200: "#a1ebc2",
+  300: "#71e2a3",
+  400: "#42d885",
+  500: "#13ce66",
+  600: "#0fa552",
+  700: "#0b7c3d",
+  800: "#085229",
+  900: "#042914",
+};
+
+const yellow = {
+  100: "#fff5d8",
+  200: "#ffebb1",
+  300: "#ffe08b",
+  400: "#ffd664",
+  500: "#ffcc3d",
+  600: "#cca331",
+  700: "#997a25",
+  800: "#665218",
+  900: "#33290c",
+};
+
 const theme = {
   colors: {
     headerBg: "#101223",
@@ -22,10 +58,13 @@ const theme = {
     btnBgHover: black[500],
     textPrimary: "#404252",
     textSecondary: "#282a3a",
-    red: "#ff4949",
-    green: "#13ce66",
+    red: red[500],
+    redHover: red[300],
+    green: green[500],
+    greenHover: green[300],
     blue: "#2d8eff",
-    yellow: "#ffcc3d",
+    yellow: yellow[500],
+    yellowHover: yellow[300],
     grey: "#f3f4f8",
     darkGrey: "#d2d4da",
     white: "#fff",
