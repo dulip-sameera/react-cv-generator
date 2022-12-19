91c16a87db9a0bb947a4585d61163aef4d997c33
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-19 14:06:10 +0530

    create a initial cv data file

---
+++ src/components/Body/data/initial_cv.js
@@ -0,0 +1,16 @@
+const initial_cv = {
+  personalInfo: {
+    firstName: "",
+    lastName: "",
+    position: "",
+    image: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+    description: "",
+  },
+  experienceList: [],
+  educationList: [],
+};
+
+export default initial_cv;
