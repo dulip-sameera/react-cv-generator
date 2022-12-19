86ad373b820660c58e61e76f1b43d4f59697876f
Dulip Sameera <dsgitrepo101@gmail.com>
2022-12-19 14:47:17 +0530

    update the function to work with cv state

--- src/components/Body/Form/Utils/updateState.js
+++ src/components/Body/Form/Utils/updateState.js
@@ -1,90 +1,68 @@
-import uniqid from "uniqid";
+import initial_education from "../../data/initial_education";
+import initial_experience from "../../data/initial_experience";
 
 function updatePersonalDataState(prevState, prop, value) {
-  prevState["data"]["personalInfo"][prop] = value;
+  prevState["personalInfo"][prop] = value;
 
   return prevState;
 }
 
 function updateExperienceDataState(prevState, id, prop, value) {
-  prevState["data"]["experienceList"] = prevState["data"]["experienceList"].map(
-    (item) => {
-      if (item.id === id) {
-        item[prop] = value;
-        return item;
-      }
-
+  prevState["experienceList"] = prevState["experienceList"].map((item) => {
+    if (item.id === id) {
+      item[prop] = value;
       return item;
     }
-  );
+
+    return item;
+  });
 
   return prevState;
 }
 
 function updateEducationDataState(prevState, id, prop, value) {
-  prevState["data"]["educationList"] = prevState["data"]["educationList"].map(
-    (item) => {
-      if (item.id === id) {
-        item[prop] = value;
-        return item;
-      }
-
+  prevState["educationList"] = prevState["educationList"].map((item) => {
+    if (item.id === id) {
+      item[prop] = value;
       return item;
     }
-  );
+
+    return item;
+  });
 
   return prevState;
 }
 
 function addToExperienceList(prevState) {
-  prevState["data"]["experienceList"] = [
-    ...prevState["data"]["experienceList"],
-    {
-      id: uniqid(),
-      position: "",
-      company: "",
-      address: "",
-      from: "",
-      to: "",
-    },
+  prevState["experienceList"] = [
+    ...prevState["experienceList"],
+    initial_experience,
   ];
 
   return prevState;
 }
 
 function addToEducationList(prevState) {
-  prevState["data"]["educationList"] = [
-    ...prevState["data"]["educationList"],
-    {
-      id: uniqid(),
-      universityName: "",
-      city: "",
-      degree: "",
-      subject: "",
-      from: "",
-      to: "",
-    },
+  prevState["educationList"] = [
+    ...prevState["educationList"],
+    initial_education,
   ];
 
   return prevState;
 }
 
 function deleteFromExperienceList(prevState, id) {
-  const index = prevState["data"]["experienceList"].findIndex(
-    (item) => item.id === id
-  );
+  const index = prevState["experienceList"].findIndex((item) => item.id === id);
 
-  prevState["data"]["experienceList"].splice(index, 1);
+  prevState["experienceList"].splice(index, 1);
 
   return prevState;
 }
 
 function deleteFromEducationList(prevState, id) {
-  const index = prevState["data"]["educationList"].findIndex(
-    (item) => item.id === id
-  );
+  const index = prevState["educationList"].findIndex((item) => item.id === id);
 
-  prevState["data"]["educationList"].splice(index, 1);
+  prevState["educationList"].splice(index, 1);
 
   return prevState;
 }
